5d4c5034d0b47fdf4eec0395b4cabc46f4ac4b8d
Nytun Jesper <nytun@insa-20666>
2026-03-17 10:36:36 +0100

    added function to change message lenght and size, and increments the character sent

--- tsock_v0.c
+++ tsock_v0.c
@@ -1,4 +1,3 @@
-
 /* librairie standard ... */
 #include <stdlib.h>
 /* pour getopt */
@@ -20,9 +19,11 @@ données du réseau */
 #include <errno.h>
 #include <string.h>
 
+#define MAX_MSG_LEN 1000
+
 /* Prototypes */
-void client_udp(int port, char* hostname); /* creates a udp client */
-void server_udp(int port);
+void client_udp(int port, char* hostname, int nbmsg, int lgmsg); /* creates a udp client */
+void server_udp(int port, int nbmsg);
 
 
 int main (int argc, char **argv) {
@@ -30,6 +31,7 @@ int main (int argc, char **argv) {
 	extern char *optarg;
 	extern int optind;
 	int nb_message = -1; /* number of messages to send, 10 by default */
+   int lg_message = -1; /* length of messages, 30 by default */
 	int source = -1 ; /* 0=reciever, 1=source */
    
    int udp = 0; /* We define a variable udp to handle the flag -u */
@@ -37,11 +39,11 @@ int main (int argc, char **argv) {
    char* hostname;
    
    
-	while ((c = getopt(argc, argv, "psun:")) != -1) {
+	while ((c = getopt(argc, argv, "psun:l:")) != -1) {
 		switch (c) {
 		case 'p':
 			if (source == 1) {
-				printf("usage: cmd [-p|-s][-n ##][-u]\n");
+				printf("usage: cmd [-p|-s][-n ##][-l ##][-u]\n");
 				exit(1);
 			}
 			source = 0;
@@ -49,7 +51,7 @@ int main (int argc, char **argv) {
 
 		case 's':
 			if (source == 0) {
-				printf("usage: cmd [-p|-s][-n ##][-u]\n");
+				printf("usage: cmd [-p|-s][-n ##][-l ##][-u]\n");
 				exit(1) ;
 			}
 			source = 1;
@@ -59,19 +61,23 @@ int main (int argc, char **argv) {
 			nb_message = atoi(optarg);
 			break;
 
+		case 'l':
+			lg_message = atoi(optarg);
+			break;         
+
       case 'u':
         udp = 1;
         break;
         
 
 		default:
-			printf("usage: cmd [-p|-s][-n ##][-u]\n");
+			printf("usage: cmd [-p|-s][-n ##][-l ##][-u]\n");
 			break;
 		}
 	}
 
 	if (source == -1) {
-		printf("usage: cmd [-p|-s][-n ##][-u]\n");     
+		printf("usage: cmd [-p|-s][-n ##][-l ##][-u]\n");    
 		exit(1) ;
 	}
 
@@ -83,7 +89,6 @@ int main (int argc, char **argv) {
 			printf("nb of messages to recieve : %d\n", nb_message);
    } else {
      if (source == 1) {
-       nb_message = 10 ;
        printf("nb of messages to send = 10 by default\n");
      } else
        printf("nb of messages to recieve = inf\n");
@@ -92,18 +97,22 @@ int main (int argc, char **argv) {
    if (udp == 1) {
      if (source == 1) {
        printf("We're on the source\n");
+
+       if (lg_message == -1) {lg_message = 30;}
+       if (nb_message == -1) {nb_message = 10;}
        
        port = atoi(argv[argc-1]); /* We assign last argument as nb port*/
        hostname = argv[argc-2]; /* We assign second to last argument as hostname*/
-       
-       client_udp(port, hostname);
+
+       /* We call on our client function */
+       client_udp(port, hostname, nb_message, lg_message);
 
      } else { 
        printf("We're on the reciever\n");
 
        port = atoi(argv[argc-1]); /* We assign last argument as nb port */
 
-       server_udp(port); 
+       server_udp(port, nb_message); 
      }
             
    } else {
@@ -114,14 +123,15 @@ int main (int argc, char **argv) {
 }
 
 
-void client_udp(int port, char* hostname){
+void client_udp(int port, char* hostname, int nbmsg, int lgmsg){
 
   int sock;
   struct sockaddr_in servaddr;
   socklen_t lg_adr_dist = sizeof(servaddr);
-  char M[30];
-  memset(M, 'a', 30);
-  struct hostent* server; 
+  char M[lgmsg];
+  char charmsg = 'a'; /* character that will be sent */
+  struct hostent* server;
+  int i = 0; /* increment */
   
   /* creating the socket of designated specs */
   if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -140,20 +150,26 @@ void client_udp(int port, char* hostname){
   servaddr.sin_port = htons(port);
   memcpy(&servaddr.sin_addr, server->h_addr, server->h_length);
 
-  /* sending */
-if (sendto(sock, M, 30, 0, (struct sockaddr *)&servaddr, lg_adr_dist) < 0) {
-    perror("sendto");
-}
-    
+  /* sending nbmsg of messages*/
+  for (i=0; i<nbmsg; i++) {
+    memset(&M, charmsg, lgmsg);
+    if (sendto(sock, M, lgmsg, 0, (struct sockaddr *)&servaddr, lg_adr_dist) < 0) {
+      perror("sendto");
+    }
+    charmsg++;
+    if (charmsg > 'z') charmsg = 'a'; /* Make sure we print characters and not whatever*/
+  }
   close(sock);
 }
 
 
-void server_udp(int port){
+void server_udp(int port, int nbmsg){
   
   int sock, n;
+  int i = 0; /* increment */
   struct sockaddr_in servaddr, cliaddr;
-  char pmsg[30];
+  char pmsg[MAX_MSG_LEN];
+  int infmessages = 0;
   socklen_t lg_adr_dist = sizeof(cliaddr);
   
   if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
@@ -172,12 +188,19 @@ void server_udp(int port){
   }
 
   /* receptiom */
-  if ((n = recvfrom(sock, pmsg, sizeof(pmsg), 0, (struct sockaddr *)&cliaddr, &lg_adr_dist)) < 0) {
-    perror("recvfrom");
-    exit(1);
+
+  if (nbmsg == -1) {infmessages = 1;}
+
+  while (infmessages || i < nbmsg) {
+    
+    if ((n = recvfrom(sock, pmsg, sizeof(pmsg), 0, (struct sockaddr *)&cliaddr, &lg_adr_dist)) < 0) {
+      perror("recvfrom");
+      exit(1);
+    }
+
+    printf("received: [%d %.*s]\n", (i+1), n, pmsg);
+    i++;
   }
-  
-  printf("received: [%.*s]\n", n, pmsg);
   
   close(sock);
 
