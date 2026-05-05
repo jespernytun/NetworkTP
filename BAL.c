5483b90727cac5438b90939acde485d60161ecd4
Jesper Nytun <160156297+jespernytun@users.noreply.github.com>
2026-05-05 12:16:13 +0200

    Update BAL.c

--- BAL.c
+++ BAL.c
@@ -3,6 +3,127 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define LG_MAX_MSG 100
+
+/* linked list data type*/
+typedef struct bal_head {
+  int nb_boites;
+  struct boite_lettre* pfirstbox;
+} bal_head;
+
+typedef struct boite_lettre {
+  int box_ID;
+  int nb_msg;
+  struct une_msg* pfirstmsg;     /* POINTS TOWARDS THE MESSAGES   */
+  struct boite_lettre* pnextbox; /* POINTS TOWARDS NEXT LETTERBOX */
+} boite_lettre;
+
+typedef struct une_msg {
+  char msg[LG_MAX_MSG]; /* FIX 1: char msg[] not char* msg[] */
+  int lg_msg;
+  struct une_msg* pnextmsg;   /* NEXT MESSAGE */
+} une_msg;
+
+/* Function that prints a message to the terminal */
+void afficher_message (char *message, int lg) {
+  int i;
+  printf("message : ");
+  for (i=0 ; i<lg ; i++) printf("%c", message[i]);
+  printf("\n");
+}
+
+
+// ADD_LETTER FUNCTION
+// FIX 6: pass lg_msg as parameter instead of using strlen()
+void add_letter(bal_head* BAL, int box_ID_dest, char* msg, int lg_msg) {
+  
+  /* making an auxilliary*/
+  boite_lettre* aux = BAL->pfirstbox;
+  boite_lettre* prev = NULL;
+
+  while (aux != NULL) {
+    if (aux->box_ID == box_ID_dest){
+      break;
+    } // BOX FOUND
+      // else
+    prev = aux;
+    aux = aux->pnextbox;
+  }
+
+  // if box not found we need to create a box
+  if (aux == NULL) {
+    boite_lettre* new_box = malloc(sizeof(boite_lettre));
+    new_box->box_ID = box_ID_dest;
+    new_box->nb_msg = 0;
+    new_box->pfirstmsg = NULL;
+    new_box->pnextbox = NULL;
+
+    if (prev == NULL) BAL->pfirstbox = new_box;
+    else prev->pnextbox = new_box;
+
+    BAL->nb_boites++;
+    aux = new_box;
+  }
+
+  // ADDING THE MESSAGE TO THE BOX
+  une_msg* new_msg = malloc(sizeof(une_msg));
+  strncpy(new_msg->msg, msg, LG_MAX_MSG);
+  new_msg->msg[LG_MAX_MSG - 1] = '\0'; 
+  new_msg->lg_msg = lg_msg;            
+  new_msg->pnextmsg = NULL;
+
+  if (aux->pfirstmsg == NULL) {
+    aux->pfirstmsg = new_msg;
+  } else {
+    une_msg* last = aux->pfirstmsg;
+    while (last->pnextmsg != NULL) last = last->pnextmsg;
+    last->pnextmsg = new_msg;
+  }
+
+  aux->nb_msg++;
+}
+
+// AFFICHE_LETTER FUNCTION
+// Sends all letters of a box over the socket, then frees the messages
+void affiche_letter(bal_head* BAL, int box_ID_dest, int sock) {
+  
+  boite_lettre* aux = BAL->pfirstbox;
+
+  /* searching box that we want to print */
+  while (aux != NULL) {
+    if (aux->box_ID == box_ID_dest) break;
+    aux = aux->pnextbox;
+  }
+
+  if (aux == NULL) {
+    printf("Sorry, no new messages for box %d\n", box_ID_dest);
+    return;
+  }
+
+  /* Send each message then free it */
+  une_msg* auxmsg = aux->pfirstmsg;
+  while (auxmsg != NULL) {
+    afficher_message(auxmsg->msg, auxmsg->lg_msg);
+
+    if (send(sock, auxmsg->msg, auxmsg->lg_msg, 0) < 0) {
+      perror("send");
+    }
+
+    une_msg* tmp = auxmsg;
+    auxmsg = auxmsg->pnextmsg;
+    free(tmp);
+  }
+
+  aux->pfirstmsg = NULL;
+  aux->nb_msg = 0;
+
+}// LINKED LISTS FOR BAL LETTER MANAGEMENT
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define LG_MAX_MSG 100
 
