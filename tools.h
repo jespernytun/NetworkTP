081fa723dedae5468c1566ad9f8cd55fa2a66ff2
Jesper Nytun <160156297+jespernytun@users.noreply.github.com>
2026-05-13 10:52:14 +0200

    Update tools.h

--- tools.h
+++ tools.h
@@ -5,22 +5,10 @@
 #include <stdio.h>
 
 /* Function that prints a message to the terminal */
-void afficher_message (char *message, int lg) {
-  int i;
-  printf("message : ");
-  for (i=0 ; i<lg ; i++) printf("%c", message[i]);
-  printf("\n");
-}
+void afficher_message (char *message, int lg)
 
-
-/*---------------------------------------------------------------------------------*/
-/* ----------------------- Message building ---------------------------------------*/
-/*---------------------------------------------------------------------------------*/
-
-void construire_message (char *message, char motif, int lg) {
-  int i;
-  for (i=0; i<lg; i++) message[i] = motif;
-}
+/* Function that creates a message */
+void construire_message (char *message, char motif, int lg)
 
 
 #endif 
