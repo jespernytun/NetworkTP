e856bddbbd599f31e3a8c29f8eea1cd5b71e4f29
Jesper Nytun <160156297+jespernytun@users.noreply.github.com>
2026-05-13 10:53:28 +0200

    Create tools.c

---
+++ tools.c
@@ -0,0 +1,14 @@
+include "tools.h"
+
+/* Function that prints a message to the terminal */
+void afficher_message (char *message, int lg) {
+  int i;
+  printf("message : ");
+  for (i=0 ; i<lg ; i++) printf("%c", message[i]);
+  printf("\n");
+}
+
+void construire_message (char *message, char motif, int lg) {
+  int i;
+  for (i=0; i<lg; i++) message[i] = motif;
+}
