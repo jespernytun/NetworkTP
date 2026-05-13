4d70ad03b43e7f1489eaadc2e2b091e36a54ce23
Jesper Nytun <160156297+jespernytun@users.noreply.github.com>
2026-05-13 12:00:16 +0200

    Update BAL-hosts.h

--- include/BAL-hosts.h
+++ include/BAL-hosts.h
@@ -28,18 +28,30 @@
 /*---------------------------------------------------------------------------------*/
 /* -----------------------BAL mode: emitter----------------------------------------*/
 /*---------------------------------------------------------------------------------*/
+// Takes a port number, a hostname, the number of messages, their length, an initialization message, and a BOX ID as inputs
+// Opens communication with BAL, sends an initialzation message that defines sizes for the correspondance and informs the BAL
+// which BAL it seeks to engage with.
 
 void bal_e(int port, char* hostname, int nbmsg, int lgmsg, struct message_init Mi, int id_recept);
+
 
 /*---------------------------------------------------------------------------------*/
 /* -----------------------BAL mode: receptor---------------------------------------*/
 /*---------------------------------------------------------------------------------*/
+// Takes a port number, a hostname, an initialization message, and a BOX ID as inputs
+// Opens communication with BAL, sends initialization meesage that informs BAL that it wants to recieve message, and from which
+// Letterbox it wants to communicate with.
 
 void bal_r(int port, char* hostname, struct message_init Mi, int id_recept);
+
 
 /*---------------------------------------------------------------------------------*/
 /* -----------------------BAL mode: BAL creation-----------------------------------*/
 /*---------------------------------------------------------------------------------*/
+// Creates a BAL at a portnumber
+// Will engage with bal_e and bal_r
+// Initializes datastructre once called upon
+// If it doesn't find a box on reception, it will create it
 
 void create_bal(int port);
 
