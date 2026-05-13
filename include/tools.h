047b239022d7fca0b331a3417c2612a6f5b0f947
Jesper Nytun <160156297+jespernytun@users.noreply.github.com>
2026-05-13 11:42:56 +0200

    Update tools.h

--- include/tools.h
+++ include/tools.h
@@ -5,10 +5,10 @@
 #include <stdio.h>
 
 /* Function that prints a message to the terminal */
-void afficher_message (char *message, int lg)
+void afficher_message (char *message, int lg);
 
 /* Function that creates a message */
-void construire_message (char *message, char motif, int lg)
+void construire_message (char *message, char motif, int lg);
 
 
 #endif 
